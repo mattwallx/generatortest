0c67d64e0b61359e8fa2f83182699e57c3b77897
mattwallx <matt.wall@qhrtech.com>
2019-07-08 11:23:31 -0700

    Add select by list of first names endpoint using PersonExample for querying

--- src/main/java/freedom/generatortest/FreedomController.java
+++ src/main/java/freedom/generatortest/FreedomController.java
@@ -3,11 +3,11 @@ package freedom.generatortest;
 
 import freedom.generatortest.dao.PersonMapper;
 import freedom.generatortest.model.Person;
+import freedom.generatortest.model.PersonExample;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,5 +28,12 @@ public class FreedomController {
     @GetMapping(value = "/people")
     public List<Person> getPeople() {
         return personMapper.selectAll();
+    }
+
+    @PostMapping(value = "/byexample")
+    public List<Person> getPersonByExample(@RequestBody List<String> names) {
+        PersonExample personExample = new PersonExample();
+        personExample.or().andFirstnameIn(names);
+        return personMapper.selectByExample(personExample);
     }
 }
